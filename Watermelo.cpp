afb8d99563cf8e31b43689d9afad775b92a26cff
Maruf <mdmarufsheikh707@gmail.com>
2025-05-18 14:49:03 +0600

    Add files via upload

---
+++ Watermelo.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+    int w;
+    cin >> w;
+    if(w % 2 == 0 && w > 2) {
+        cout << "YES" <<endl; 
+    } else {
+        cout << "NO" <<endl;
+    }
+    return 0;
+}
