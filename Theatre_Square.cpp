79732af52bfae7e0e5fe538cc7550706cce8c285
Maruf <mdmarufsheikh707@gmail.com>
2025-05-20 22:17:12 +0600

    Add files via upload

--- Theatre_Square.cpp
+++ Theatre_Square.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 int main() {
     optimize();
-    int n, m, a;
+    long long int n, m, a;
     cin >> n >> m >> a;
     int result_1 = (n + a - 1) / a;
     int result_2 = (m + a - 1) / a;
