002d65fe67e3e67f15ead1185cd62be5f2a9184a
Maruf <mdmarufsheikh707@gmail.com>
2025-05-22 22:52:01 +0600

    Add files via upload

---
+++ Domino_piling.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int main() {
+    optimize();
+    int x, y;
+    cin >> x >> y;
+    cout << (x * y ) / 2 << endl;
+    return 0;
+}
