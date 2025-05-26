4e33f189e8a8d0b7468c989e52221108a77ee800
Maruf <mdmarufsheikh707@gmail.com>
2025-05-26 13:19:42 +0600

    Add files via upload

---
+++ Elephant.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize(); ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() 
+{
+    optimize();
+    
+    int x;
+    cin >> x;
+    
+    cout << (x + 5 - 1) / 5 << endl;
+
+    return 0;
+}
